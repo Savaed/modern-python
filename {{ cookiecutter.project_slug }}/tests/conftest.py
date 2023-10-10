d61db30fbf068d8ac15b71d76de277fca09f041f
Krzysztof Gonet <36476874+Savaed@users.noreply.github.com>
2023-10-10 11:35:09 +0200

    feat: add hypothesis profiles

--- {{ cookiecutter.project_slug }}/tests/conftest.py
+++ {{ cookiecutter.project_slug }}/tests/conftest.py
@@ -1,11 +1,25 @@
+{% if cookiecutter.use_hypothesis -%}
+import os
+{% endif -%}
 from typing import Any, Iterator
 
 import pytest
+{%- if cookiecutter.use_hypothesis %}
+from hypothesis import settings
+{% endif %}
 from loguru import logger
 from pytest import LogCaptureFixture
 
 from {{cookiecutter.project_slug}}.log import format_key_value_context
+{% if cookiecutter.use_hypothesis %}
 
+settings.register_profile("ci", max_examples=250)
+
+if os.getenv("CI", False):
+    settings.load_profile("ci")
+else:
+    settings.load_profile("default")
+{% endif %}
 
 @pytest.fixture
 def sample_global_fixture(request: Any) -> Any:
